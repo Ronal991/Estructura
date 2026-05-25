5b982d3e8b2b7589d2ecab2ee315e0f985629ada
Ronald Arboleda <anguloangulo452@gmail.com>
2026-05-25 12:00:09 -0500

    Y ya.

--- Merge-sort/merge.c
+++ Merge-sort/merge.c
@@ -1,82 +1,75 @@
-﻿#include <stdio.h>
-#include <stdlib.h>
-
+#include <stdio.h>
 // Función para fusionar dos mitades ordenadas del arreglo
 void merge(int arr[], int left, int mid, int right) {
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-    int L[n1], R[n2];
-    for (int i = 0; i < n1; i++)
-        L[i] = arr[left + i];
-    for (int j = 0; j < n2; j++)
-        R[j] = arr[mid + 1 + j];
-
-    int i = 0, j = 0, k = left;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k++] = L[i++];
-        } else {
-            arr[k++] = R[j++];
-        }
-    }
-
-    while (i < n1)
-        arr[k++] = L[i++];
-    while (j < n2)
-        arr[k++] = R[j++];
+ int n1 = mid - left + 1;
+ int n2 = right - mid;
+ int L[n1], R[n2];
+ for (int i = 0; i < n1; i++) L[i] = arr[left + i];
+ for (int j = 0; j < n2; j++) R[j] = arr[mid + 1 + j];
+ int i = 0, j = 0, k = left;
+ while (i < n1 && j < n2) {
+ if (L[i] <= R[j]) {
+ arr[k++] = L[i++];
+ } else {
+ arr[k++] = R[j++];
+ }
+ }
+ while (i < n1) arr[k++] = L[i++];
+ while (j < n2) arr[k++] = R[j++];
 }
-
 // Función recursiva de Merge Sort
 void mergeSort(int arr[], int left, int right) {
-    if (left < right) {
-        int mid = (left + right) / 2;
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-        merge(arr, left, mid, right);
-    }
+if (left < right)
+{
+ int mid = (left + right) / 2;
+ mergeSort(arr, left, mid);
+ mergeSort(arr, mid + 1, right);
+ merge(arr, left, mid, right);
+ }
 }
-
 int main() {
-    int n;
-    char continuar = 's';
+ char continuar = 's';
 
-    printf("Bienvenido a la organizadora de datos\n");
+ while (continuar == 's' || continuar == 'S') {
+ printf("---------Bienvenido a la organizadora de datos--------\n");
+ printf("Que datos quieres organizar?\n");
 
-    while (continuar == 's' || continuar == 'S') {
-        printf("Cuantos numeros desea organizar?: ");
-        if (scanf("%d", &n) != 1 || n <= 0) {
-            printf("Entrada invalida. Ingrese un numero entero positivo.\n");
-            int ch;
-            while ((ch = getchar()) != '\n' && ch != EOF);
-            continue;
-        }
+ int n;
+ int valid = 0;
+ while (!valid) {
+ printf("Ingrese la cantidad de valores a ordenar: ");
+ if (scanf("%d", &n) != 1 || n <= 0) {
+ int c;
+ while ((c = getchar()) != '\n' && c != EOF);
+ printf("Cantidad no valida. Intente de nuevo.\n");
+ } else {
+ valid = 1;
+ }
+ }
 
-        int *data = malloc(n * sizeof(int));
-        if (data == NULL) {
-            printf("Error de memoria. Saliendo.\n");
-            return 1;
-        }
+ int data[n];
+ for (int i = 0; i < n; i++) {
+ printf("Valor %d: ", i + 1);
+ scanf("%d", &data[i]);
+ }
 
-        for (int i = 0; i < n; i++) {
-            printf("Ingrese el numero %d: ", i + 1);
-            scanf("%d", &data[i]);
-        }
+ mergeSort(data, 0, n - 1);
+ printf("\nDatos ordenados:\n");
+ for (int i = 0; i < n; i++) {
+ printf("%d ", data[i]);
+ }
+ printf("\n");
 
-        mergeSort(data, 0, n - 1);
-
-        printf("Arreglo ordenado: ");
-        for (int i = 0; i < n; i++) {
-            printf("%d ", data[i]);
-        }
-        printf("\n");
-
-        free(data);
-
-        printf("Desea continuar organizando numeros? (s/n): ");
-        scanf(" %c", &continuar);
-        int ch;
-        while ((ch = getchar()) != '\n' && ch != EOF);
-    }
-
-    return 0;
+ printf("Deseas continuar organizando datos? (s/n): ");
+ int c;
+ while ((c = getchar()) != '\n' && c != EOF);
+ continuar = getchar();
+ while (continuar == '\n') {
+ continuar = getchar();
+ }
+ if (continuar == 'S' || continuar == 's') {
+ while ((c = getchar()) != '\n' && c != EOF);
+ }
+ }
+ return 0;
 }
